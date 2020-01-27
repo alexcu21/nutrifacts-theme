10413189892aaf61e6e3249fbc99b694e4ceecf5
Alex Cuadra <alexcu21@live.com>
2020-01-27 17:43:44 -0600

    fixing footer

--- footer.php
+++ footer.php
@@ -1,33 +1,11 @@
-<footer class="footer-demo section-dark">
+<footer class="section-dark">
     <div class="container">
-        <nav class="pull-left">
-            <ul>
-
-                <li>
-                    <a href="http://www.creative-tim.com">
-                        Creative Tim
-                    </a>
-                </li>
-                <li>
-                    <a href="http://blog.creative-tim.com">
-                       Blog
-                    </a>
-                </li>
-                <li>
-                    <a href="http://www.creative-tim.com/product/rubik">
-                        Licenses
-                    </a>
-                </li>
-            </ul>
-        </nav>
-        <div class="copyright pull-right">
-            &copy; 2015, made with <i class="fa fa-heart heart"></i> by Creative Tim
+        <div class="copyright text-center">
+            &copy; <?php the_date('Y') ?>, made with <i class="fa fa-heart heart"></i> by <a href="http://alexcuadra.server2.demoswp.com/">Alex Cuadra</a>
         </div>
     </div>
 </footer>
-<!-- Modal Bodies come here -->
 
-<!--   end modal -->
 
 
 </body>
